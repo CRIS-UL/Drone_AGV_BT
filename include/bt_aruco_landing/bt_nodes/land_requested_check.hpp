012b296edbc02f63c3c5017bcf4276cdd71f6dd7
Tom Dorian <tom.dorian@eiratech.com>
2025-11-11 12:37:52 +0000

    Adding Enable Joysticks

--- include/bt_aruco_landing/bt_nodes/land_requested_check.hpp
+++ include/bt_aruco_landing/bt_nodes/land_requested_check.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <behaviortree_cpp_v3/action_node.h>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/empty.hpp>
 namespace bt_aruco_landing
 {
 class LandRequestedCheck : public BT::SyncActionNode
@@ -11,5 +13,8 @@ public:
     static BT::PortsList providedPorts(){
         return{};
     }  
+private:
+    bool prev_land_requested_;
+    rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr enable_virtual_sticks_pub_;
 };
 } //namespace bt_aruco_landing
--- src/bt_nodes/land_requested_check.cpp
+++ src/bt_nodes/land_requested_check.cpp
@@ -3,6 +3,9 @@ namespace bt_aruco_landing
 {
 LandRequestedCheck::LandRequestedCheck(const std::string& name, const BT::NodeConfiguration& config)
     : BT::SyncActionNode(name, config){
+        prev_land_requested_ = false;
+        enable_virtual_sticks_pub_ = config.blackboard->get<rclcpp::Node::SharedPtr>("node")->create_publisher<std_msgs::msg::Empty>(
+            "/simple_drone/enable_virtual_sticks", 10);
 }
 BT::NodeStatus LandRequestedCheck::tick()
 {
@@ -11,8 +14,15 @@ BT::NodeStatus LandRequestedCheck::tick()
     bool land_requested = blackboard->get<bool>("land_requested");
     
     if (land_requested) {
+        if(prev_land_requested_){
+            std_msgs::msg::Empty msg;
+            enable_virtual_sticks_pub_->publish(msg);
+        }
+        prev_land_requested_ = true;
+
         return BT::NodeStatus::SUCCESS;
     } else {
+        prev_land_requested_ = false;
         return BT::NodeStatus::FAILURE;
     }
 }
